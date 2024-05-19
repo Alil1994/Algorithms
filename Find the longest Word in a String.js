0f03379471ded835d053841c173b8383a3b1aa4a
Ali Al-dubai <167811136+Alil1994@users.noreply.github.com>
2024-05-19 16:37:43 +0200

    added all files

---
+++ Find the longest Word in a String.js
@@ -0,0 +1,15 @@
+ //Find the longest Word in a String :
+
+ function findLongestWord(sentence) {
+    let words = sentence.split(' ');
+    let longestLength = 0;
+    for (let i = 0; i < words.length; i++) {
+      if (words[i].length > longestLength) {
+        longestLength = words[i].length;
+      }
+    }
+    
+    return longestLength;
+  }
+  console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));  
+  console.log(findLongestWord("May the force be with you"));
